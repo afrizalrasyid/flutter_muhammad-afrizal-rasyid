1bb2ff16479d6efb432deb198bee9ee8b21faa8a
rasyid <afrizalrasyid461@gmail.com>
2023-09-02 07:05:23 +0700

    Introduction Flutter

---
+++ 08_Introduction Flutter Widget/praktikum/latihan/rasyid/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HelloWorld());
+}
+
+class HelloWorld extends StatelessWidget {
+const HelloWorld({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(      
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Hello World!'
+            )
+          ),
+          body: const Text('test'),
+        ),
+    );
+  }
+}
