90449dfd25d19b419eb8b323851419b0a8d067b1
rasyid <afrizalrasyid461@gmail.com>
2023-09-09 15:10:05 +0700

    Latihan

--- 08_Introduction Flutter Widget/praktikum/latihan/rasyid/lib/main.dart
+++ 08_Introduction Flutter Widget/praktikum/latihan/rasyid/lib/main.dart
@@ -16,7 +16,28 @@ const HelloWorld({ Key? key }) : super(key: key);
           ),
         body: const Center(
           child: Text("material app"),
-        )
+        ),
+        drawer:  Drawer(
+          child:  ListView(
+            children: const [
+              DrawerHeader(         
+                decoration: BoxDecoration(color: Colors.blue),       
+                child: Text('Drawer Header')
+              ),
+              ListTile(title: Text('List Tile 1'),
+              ),
+              ListTile(title: Text('List Tile 2'),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: 'home'),
+            NavigationDestination(icon: Icon(Icons.settings), label: 'settings'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'profile')
+          ],
+        ),
       )
     );
   }
