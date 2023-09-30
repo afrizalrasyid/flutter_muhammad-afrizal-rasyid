905d90f9b09f75c748da79e0a3b8bf85e732173f
rasyid <afrizalrasyid461@gmail.com>
2023-09-30 14:37:00 +0700

    Code Competence

--- Weekly1/project/lib/main.dart
+++ Weekly1/project/lib/main.dart
@@ -6,13 +6,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(          
+        appBar: AppBar(
           backgroundColor: Colors.grey.shade800,
           title: Row(
             children: [
@@ -24,7 +24,9 @@ class MyApp extends StatelessWidget {
               const SizedBox(width: 10.0),
               const Text(
                 'Our Furniture',
-                style: TextStyle(fontSize: 20,), 
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ],
           ),
@@ -34,36 +36,32 @@ class MyApp extends StatelessWidget {
             children: [
               const Stack(
                 children: <Widget>[
-                  Image(                    
+                  Image(
                     image: AssetImage('assets/images/header_couch.jpg'),
                   ),
                   Positioned(
-                    left: 100,
-                    top: 35,
-                    child: Text(
-                      'Welcome To Our Furniture',
-                      style: TextStyle(
-                        fontSize: 19,
-                        fontWeight: FontWeight.w600
-                      ),
-                    )
-                  ),
+                      left: 100,
+                      top: 35,
+                      child: Text(
+                        'Welcome To Our Furniture',
+                        style: TextStyle(
+                            fontSize: 19, fontWeight: FontWeight.w600),
+                      )),
                   Positioned(
-                    left: 140,
-                    top: 60,
-                    child: Text(
-                      'Comfort is our priority',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w300
-                      ),
-                    )
-                  )
+                      left: 140,
+                      top: 60,
+                      child: Text(
+                        'Comfort is our priority',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w300),
+                      ))
                 ],
               ),
               Row(
                 children: [
-                  const SizedBox(width: 20,),
+                  const SizedBox(
+                    width: 20,
+                  ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -75,38 +73,38 @@ class MyApp extends StatelessWidget {
                         ),
                       ),
                       const Text('Find everything you need'),
-                      ElevatedButton(                        
-                        onPressed: (){},                         
+                      ElevatedButton(
+                        onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey.shade800
-                        ),
+                            backgroundColor: Colors.amber),
                         child: const Text(
                           'More',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
+                          style: TextStyle(color: Colors.black),
                         ),
                       )
                     ],
                   ),
-                  const SizedBox(width: 35,),
+                  const SizedBox(
+                    width: 35,
+                  ),
                   const Image(
-                    height: 358,                                        
+                    height: 358,
                     image: AssetImage(
                       'assets/images/section_1.jpg',
-                    )
+                    ),
                   )
                 ],
               ),
               Row(
-                children: [                  
+                children: [
                   const Image(
-                    height: 314,                                        
-                    image: AssetImage(
-                      'assets/images/section_2.jpg',
-                    )
+                      height: 314,
+                      image: AssetImage(
+                        'assets/images/section_2.jpg',
+                      )),
+                  const SizedBox(
+                    width: 30,
                   ),
-                  const SizedBox(width: 30,),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
@@ -124,28 +122,29 @@ class MyApp extends StatelessWidget {
                           Text('place'),
                         ],
                       ),
-                      ElevatedButton(                        
-                        onPressed: (){},                         
+                      ElevatedButton(
+                        onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey.shade800
-                        ),
+                            backgroundColor: Colors.amber),
                         child: const Text(
                           'Store',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
+                          style: TextStyle(color: Colors.black),
                         ),
                       )
                     ],
                   ),
                 ],
               ),
-              const SizedBox(height: 70,),
+              const SizedBox(
+                height: 70,
+              ),
               const Row(
                 children: [
-                  SizedBox(width: 20,),
+                  SizedBox(
+                    width: 20,
+                  ),
                   Text(
-                    'Contact us',                  
+                    'Contact us',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
@@ -153,40 +152,146 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 30,),
               const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(width: 22,),
-                  Text(
-                    'Need to get in touch with us? Either fill out the form with your',                  
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
+                  SizedBox(
+                    width: 22,
                   ),
-                ],
-              ),              
-              const Row(
-                children: [
-                  SizedBox(width: 22,),
-                  Text(
-                    'inquiry or find the department email you like to contact below.',                  
-                    style: TextStyle(
-                      fontSize: 12,
+                  SizedBox(
+                    width: 335,
+                    child: Text(
+                      'Need to get in touch with us? Either fill out the form with your inquiry or find the departement email you like to contact below.',
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 30,
+              ),
               const Row(
                 children: [
-                  SizedBox(width: 20,),
+                  SizedBox(
+                    width: 20,
+                  ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('First name*'),
-                      Form(child: Text('test'))
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: 180,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 228, 228, 228),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
-                  Text('Last name'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Last Name'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: 180,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 228, 228, 228),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Email*'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: 370,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 228, 228, 228),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('What can we help you with?'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: 370,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 228, 228, 228),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                    onPressed: () {},
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  )
                 ],
               )
             ],
